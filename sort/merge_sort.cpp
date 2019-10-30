226b6e818635c2c4f64702d4503a3492a747298d
muzhi <muzhi@muzhideMacBook-Pro.local>
2019-10-30 20:58:50 +0800

    not matter

---
+++ sort/merge_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+/*
+ * in-place
+**/
+template <typename T>
+void MergeSortMain(std::vector<T>& arr)
+{
+    typedef typename std::vector<T>::size_type size_type;
+
+    if (arr.empty())
+        std::cout << "The array is empty" << std::endl;
+    else {
+
+    }
+}
+
+template <typename T>
+void MergeSort(std::vector<T>& arr, )
