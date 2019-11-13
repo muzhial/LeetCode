e180c1d4890a02e133ddacfceb5c339e2e9de261
muzhi <kantchenjn@163.com>
2019-11-13 20:33:03 +0800

    not matter

--- cpp/RegularMatch.cpp
+++ cpp/RegularMatch.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 
 class Solution {
@@ -14,47 +14,48 @@ public:
     bool Match(std::string &s, std::string &p,
             std::string::size_type s_idx, std::string::size_type p_idx)
     {
-        if (s_idx == s.size() && p_idx == p.size())
+        if (s_idx >= s.size() && p_idx >= p.size())
             return true;
-        while (p_idx != p.size()) {
-            if (s_idx != s.size()) {
-                if (p_idx+1 != p.size() && p[p_idx+1] == '*') {
-                    p_idx += 2;
-                    if (Match(s, p, s_idx, p_idx))
-                        return true;
-                    else
-                        return false;
-                }
-                else if (p[p_idx] == '.') {
-                    ++p_idx;
-                    ++s_idx;
-                }
-                else if (p[p_idx] == s[s_idx]) {
-                    ++p_idx;
-                    ++s_idx;
-                }
-                else
-                    return false;
+        else if (p_idx+1 < p.size() && p.at(p_idx+1) == '*') {
+            if (Match(s, p, s_idx, p_idx+2))
+                return true;
+            if (s_idx < s.size() && p.at(p_idx) == s.at(s_idx)
+                    || s_idx < s.size() && p.at(p_idx) == '.') {
+
+                return Match(s, p, s_idx+1, p_idx);
             }
-            else {
-                if (p_idx+2 == p.size() && p[p_idx+1] == '*')
-                    return true;
-                else
-                    return false;
-            }
+        } else if (s_idx < s.size() && p_idx < p.size() 
+                && (p.at(p_idx) == s.at(s_idx) || p.at(p_idx) == '.')) {
+
+            return Match(s, p, s_idx+1, p_idx+1);
+        } else {
+            return false;    
         }
-        if (s_idx != s.size())
-            return false;
     }
+};
+
+// another solution --> DP algorithm
+class SolutionI {
+
 };
 
 int main()
 {
-    std::string s("kjh");
-    std::string p("kjhass");
+    std::vector<std::string> col{"kjh",      "aas", "", \
+        "",  "s", "aklsw", "",   "aaa", "bbb", "aaab", \
+        "aaabcdwww", "ab", "mississippi", "bbabacccbcbbcaaab"};
+    std::vector<std::string> col_p{"kjhass", "a",   "", \
+        "a", "",  "aklsw", "a*", "a*",  "b*b", "a*b",  \
+        "a*b*cdw*",  ".*", "mis*is*p*.",  "a*b*a*a*c*aa*c*bc*"};
+    std::vector<std::string>::size_type i = 0;
+    auto sz = col.size();
+
     Solution so;
-    std::cout << "src: " << s
-              << "\npat: " << p << std::endl;
-    std::cout << ">>> " << (so.IsMatch(s, p) ? "true" : "false") << std::endl;
+    for (; i < sz; ++i) {
+        std::cout << "src: " << col.at(i)
+                  << "\npat: " << col_p.at(i) << std::endl;
+        std::cout << ">>> " << (so.IsMatch(col.at(i), col_p.at(i)) ? "true" : "false") << std::endl;
+    }
+
     return 0;
 }
