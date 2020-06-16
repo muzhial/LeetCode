cb7204e93543e738a2c374765e9530b0b0c2544b
muzhi <muzhi@muzhideMacBook-Pro.local>
2020-06-16 23:56:36 +0800

    tree 2

--- cpp/tree/124_BTMaxPathSum.cpp
+++ cpp/tree/124_BTMaxPathSum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 using namespace std;
 using std::vector;
 
@@ -13,12 +14,32 @@ struct TreeNode {
 };
 
 /***************
+ * [Hint]
+ * 题目对 path 的描述不清晰，注意 path 中不会出现分叉，比如：
+ *          5
+ *        /   \
+ *      4      8         is 48 not 55
+ *    /       /  \
+ *   11      13   4
+ * 7    2          1
 ****************/
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        
+        helper(root);
+        return max_;
     }
+    int helper(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left = std::max(0, helper(root->left));
+        int right = std::max(0, helper(root->right));
+        max_ = std::max(max_, left + right + root->val);
+        return std::max(left, right) + root->val;
+    }
+private:
+    int max_ = std::numeric_limits<int>::min();
 };
 /***************
 ****************/
