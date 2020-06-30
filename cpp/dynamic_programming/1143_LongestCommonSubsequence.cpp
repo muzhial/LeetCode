ed7a58be0e2205b95493ff14c7f5e5f5a9a78bed
muzhi <muzhi@muzhideMacBook-Pro.local>
2020-06-30 23:54:52 +0800

    dp

--- cpp/dynamic_programming/1143_LongestCommonSubsequence.cpp
+++ cpp/dynamic_programming/1143_LongestCommonSubsequence.cpp
@@ -8,14 +8,14 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
-        int n = text1.size();
+        int n = text2.size();
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
         for (int i = 1; i < m + 1; ++i) {
             for (int j = 1; j < n + 1; ++j) {
-                if (text1[i - 1] == text2[ j - 1]) {
-                    dp[i][j] = dp[i][j - 1] + 1;
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-
+                    dp[i][j] = std::max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
