27e2722d19f2582e9b8b4be6e5aafe825d46f80d
muzhichan <muzhi@163.com>
2020-07-06 14:59:23 +0800

    dynamic programming complete

--- cpp/dynamic_programming/650_2KeysKeyboard.cpp
+++ cpp/dynamic_programming/650_2KeysKeyboard.cpp
@@ -7,6 +7,17 @@ using std::vector;
 class Solution {
 public:
     int minSteps(int n) {
-        
+        vector<int> dp(n + 1, 0);
+        int h = std::sqrt(n);
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = i;
+            for (int j = 2; j <= h; ++j) {
+                if (i % j == 0) {
+                    dp[i] = dp[j] + dp[i / j];
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 };
