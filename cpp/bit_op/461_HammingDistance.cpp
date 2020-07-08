b3d656df28877bfe741ccf0c430eee62c5955573
muzhichan <kantchenjn@163.com>
2020-07-08 17:26:54 +0800

    bit op1

--- cpp/bit_op/461_HammingDistance.cpp
+++ cpp/bit_op/461_HammingDistance.cpp
@@ -17,3 +17,17 @@ public:
         return cnt;
     }
 };
+
+class SolutionI {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        int cnt = 0;
+        while (z != 0) {
+            // 去除 n 的位级表示中最低的那一位 1
+            z = z & (z - 1);
+            cnt++;
+        }
+        return cnt;
+    }
+};
